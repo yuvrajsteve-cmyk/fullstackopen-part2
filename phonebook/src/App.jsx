91ed68688002432b59748575f6511be9af929b63
Yuvraj Singh <yuvrajsteve@email.com>
2026-06-14 22:08:04 +0530

    2.8: The Phonebook Step 3

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -9,26 +9,36 @@ const App = () => {
   ])
 
   const [newName, setNewName] = useState('')
+  const [number, setNmber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
 
   const nameExist = persons.some(
     (person) => person.name.toLocaleLowerCase() === newName.toLocaleLowerCase()
+    
   ) 
   if(nameExist) {
     alert(`${newName} is already added in the phonebook`)
     return
   } 
+
+  
     const personObject = {
-      name: newName
+      name: newName,
+      number: number
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleAddNumber = (event) => {
+    setNmber(event.target.value)
   }
   return (
     <div>
@@ -38,13 +48,16 @@ const App = () => {
             name: <input value={newName} onChange={handleNameChange}/>
           </div>
           <div>
+            number: <input value={number} onChange={handleAddNumber}/>
+          </div>
+          <div>
             <button type="submit">add</button>
           </div>
          </form>
          <h2>numbers</h2>
          <ul>
           {persons.map(person => 
-            <li key={person.name}>{person.name}</li>
+            <li key={person.name}>{person.name} {person.number}</li>
           )}
          </ul>
     </div>
