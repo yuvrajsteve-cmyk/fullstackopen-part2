13978f53e8a1ea5584d8b2de379c29c385568f7e
Yuvraj Singh <yuvrajsteve@email.com>
2026-06-20 03:06:51 +0530

    this is for practice

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -2,30 +2,33 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 
 const App = () => {
-  
+ 
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  
+ 
+  const [notes, setNotes] = useState([]) 
+  const [newNote, setNewNote] = useState('') 
 
   
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
-      })
+    
+    axios.get('http://localhost:3001/persons').then(response => {
+      setPersons(response.data)
+    })
   }, [])
 
- 
+  
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
+  
 
  
   const addName = (event) => {
     event.preventDefault()
 
-   
-    const nameExists = persons.some(p => p.name.toLowerCase() === newName.toLowerCase())
+    const nameExists = persons.some(p => p.name?.toLowerCase() === newName.toLowerCase())
     if (nameExists) {
       alert(`${newName} is already added to phonebook`)
       return
@@ -34,38 +37,54 @@ const App = () => {
     const personObject = {
       name: newName,
       number: newNumber,
-      id: String(persons.length + 1)
+      id: String(new Date().getTime())
     }
 
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
+    axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
   }
+    const deletePerson = (id, name) => {
+      if (window.confirm(`Delete ${name}?`)) {
+        axios
+        .delete(`http://localhost:3001/persons/${id}`)
+        .then(() => {
+          setPersons(persons.filter(person => person.id !== id))
+        })
+        .catch(error => {
+          alert(`This person was already deleted from the server`)
+          setPersons(persons.filter(person => person.id !== id))
+        })
+      }
+    }
 
   return (
     <div>
-      <h1>PhoneBook</h1>
-
      
+      <h1>PhoneBook</h1>
       <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div>name: <input value={newName} onChange={handleNameChange} /></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+        <button type="submit">add person</button>
       </form>
 
       <h2>Numbers</h2>
       <ul>
-       
         {persons.map(person => 
-          <li key={person.id}>{person.name} {person.number}</li>
+          <li key={person.id}>{person.name} {person.number} {' '}
+          <button onClick={() => deletePerson(person.id, person.name)}>delete</button>
+          </li>
         )}
       </ul>
+
+      <hr /> 
+
+      
+      
     </div>
   )
 }
