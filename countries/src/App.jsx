9ae90bcac4dded64258b49cdfea112752a2ab160
Yuvraj Singh <yuvrajsteve@email.com>
2026-06-21 14:14:01 +0530

    2.19*: Data for countries, step 2

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
 
   const [searchQuery, setSearchQuery] = useState('')
   const [allCountries, setAllCountries] = useState([])
+  const [selectedCountry, setSelectedCountry] = useState(null)
 
 
   useEffect (() => {
@@ -18,6 +19,7 @@ const App = () => {
 
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value)
+    setSelectedCountry(null)
   }
 
   const countriesToShow = allCountries.filter(country => 
@@ -25,19 +27,35 @@ const App = () => {
   )
 
   let renderContent = null
-  if (countriesToShow.length > 10) {
+  if(selectedCountry !== null) {
+    renderContent = <div>
+      <h1>{selectedCountry.name.common}</h1>
+      <p>capital {selectedCountry.capital?.[0]}</p>
+      <p>area {selectedCountry.area}</p>
+      <h1>Languages :</h1> 
+      <ul>
+      {Object.values(selectedCountry.languages).map (lang => 
+        <li key={lang}>{lang}</li>
+      )}
+      </ul>
+      <img src={selectedCountry.flags.png} alt={`flag of ${selectedCountry.name.common}`} />
+    </div>
+  }
+  else if (countriesToShow.length > 10) {
     renderContent = <p>Too many matches, specify another filter</p>
   } else if (countriesToShow.length <= 10 && countriesToShow.length > 1) {
     renderContent = <ul>
       {countriesToShow.map(country => 
-        <li key={country.name.common}>{country.name.common}</li>
+        <li key={country.name.common}>{country.name.common}
+        <button onClick={() => setSelectedCountry(country)}>show</button>
+        </li>
       )}
     </ul>
   } else if (countriesToShow.length === 1) {
     const country = countriesToShow[0]
     renderContent = <div>
       <h1>{country.name.common}</h1>
-      <p>capital {country.capital[0]}</p>
+      <p>capital {country.capital?.[0]}</p>
       <p>area {country.area}</p>
       <h1>Languages :</h1> 
       <ul>
